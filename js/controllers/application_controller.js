6aee3ffcd0f69d80f4a2cb8cdc879b81cbc9ed0c
Mehul Kar <mehul.kar@gmail.com>
2012-11-26 09:03:16 -0800

    move application controller out to another dir

---
+++ js/controllers/application_controller.js
@@ -0,0 +1,1 @@
+Piano.ApplicationController = Ember.Controller.extend()
--- js/piano.js
+++ js/piano.js
@@ -1,12 +1,8 @@
-
 (function( win ) {
 	'use strict';
-
 	win.Piano = Ember.Application.create({
 		VERSION: '1.0',
-		rootElement: '#piano',
-		// Extend to inherit outlet support
-		ApplicationController: Ember.Controller.extend()
+		rootElement: '#piano'
 	});
 
 })( window );
