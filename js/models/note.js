cf74f7f501363771cb1ad61e38b38af130523c84
Mehul Kar <mehul.kar@gmail.com>
2013-01-19 21:07:29 -0800

    we don't need all these extra files

--- js/models/note.js
+++
@@ -1,4 +0,0 @@
-Piano.Note = Ember.Object.extend({
-  name: null,
-  value: null
-})
--- js/piano.js
+++
@@ -1,8 +0,0 @@
-(function( win ) {
-	'use strict';
-	win.Piano = Ember.Application.create({
-		VERSION: '1.0',
-		rootElement: '#piano'
-	});
-
-})( window );
