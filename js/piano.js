34de9b3733122a51ff3ba076a381709e3550f38e
Mehul Kar <mehul.kar@gmail.com>
2013-09-10 22:59:08 -0700

    basic scale

--- js/piano.js
+++ js/piano.js
@@ -1,1 +1,21 @@
 Piano = Ember.Application.create();
+
+Piano.ApplicationRoute = Ember.Route.extend({
+  action: {
+    activate: function(controller) {
+      controller.set('currentNote', 1);
+    }
+  }
+});
+
+Piano.ApplicationController = Ember.Controller.extend({
+  currentNote: 0,
+
+  updateCurrentNote: function() {
+    var note = this.get('currentNote');
+    if (note < 8) {
+      this.incrementProperty('currentNote');
+      Ember.run.later(this, 'updateCurrentNote', 1000);
+    }
+  }.on('init')
+});
