052097e5bdd63f77e679bbb3ddb3338697f0b501
Mehul Kar <mehul.kar@gmail.com>
2013-09-10 22:44:27 -0700

    reset with ember 1.0

--- js/piano.js
+++ js/piano.js
@@ -1,13 +1,1 @@
 Piano = Ember.Application.create();
-
-Piano.Router.map(function() {
-  this.route('/');
-  this.route('current');
-
-});
-
-Piano.IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('current');
-  }
-});
