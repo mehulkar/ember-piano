f9f8727cc06d4a29ca9a2f9921415ce6c86b3db2
Mehul Kar <mehul.kar@gmail.com>
2013-09-10 23:17:06 -0700

    basic scale with letter notes

--- js/piano.js
+++ js/piano.js
@@ -1,21 +1,43 @@
 Piano = Ember.Application.create();
 
 Piano.ApplicationRoute = Ember.Route.extend({
-  action: {
-    activate: function(controller) {
-      controller.set('currentNote', 1);
-    }
+  model: function(controller) {
+    this._super(controller);
+    return Piano.Note.create({val: 0});
   }
 });
 
 Piano.ApplicationController = Ember.Controller.extend({
-  currentNote: 0,
+  currentNote: Em.computed.alias('content'),
 
   updateCurrentNote: function() {
     var note = this.get('currentNote');
-    if (note < 8) {
-      this.incrementProperty('currentNote');
+
+    if (note && note.get('val') < 8) {
+      this.incrementProperty('currentNote.val');
+
+      // and run it again after 1 second
       Ember.run.later(this, 'updateCurrentNote', 1000);
     }
-  }.on('init')
+
+  }.observes('content').on('init')
+});
+
+var noteMapping = {
+    1: 'C',
+    2: 'D',
+    3: 'E',
+    4: 'F',
+    5: 'G',
+    6: 'A',
+    7: 'B',
+    8: 'C'
+};
+
+Piano.Note = Ember.Object.extend({
+  val: 0,
+
+  note: function() {
+    return noteMapping[this.get('val')]
+  }.property('val')
 });
